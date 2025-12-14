116daf6f565783fe327bb3b0be1cca6d8a7797bd
nasta0904 <nastey09042010@gmail.com>
2025-12-14 14:23:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+    initializeNavigation();
+    // ... остальной код инициализации
+    
+    // Показываем домашнюю страницу
+    showSection('home');
+});
